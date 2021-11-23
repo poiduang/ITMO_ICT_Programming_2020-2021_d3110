7d7b9bd3bd3a4b437f59802535cb9dd2f10461f6
poiduang <1159649945@qq.com>
2021-11-23 03:17:35 +0300

    更新 Цезаря.py
    Цезаря

--- students/D3110/Чжао Цзыхао/lr1/Цезаря.py
+++ students/D3110/Чжао Цзыхао/lr1/Цезаря.py
@@ -1,6 +1,6 @@
-ptxt=input("请输入一串字符：")
+xxxx=input("请输入一串字符：")
 key=input("请输入位移长度")
-for c in ptxt:
+for c in xxxx:
       if "a"<=c<="z":
             print(chr(ord("a")+(ord(c)-ord("a")+int(key))%26),end="")
       elif "A"<=c<="Z":
